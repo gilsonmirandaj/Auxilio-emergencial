01278094b1087de66501e51b79cb06589a4eff2a
gilsonmirandaj <gilsonmirandajunior2@gmail.com>
2021-05-11 16:39:09 -0300

    mães

---
+++ maesSolteiras/maeSolteiraComBf.java
@@ -0,0 +1,15 @@
+package classeMaeSolteira;
+
+
+	public class maeSolteiraComBf extends PessoasComBF{
+		private char filhos;
+
+		public char getFilhos() {
+			return filhos;
+		}
+
+		public void setFilhos(char filhos) {
+			this.filhos = filhos;
+		}
+
+}
